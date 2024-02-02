93a976bcd2c58caab340389d51a3c205dfc5081a
kevin josue <francodelgadojj@gmail.com>
2024-02-02 16:57:41 -0500

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+@app.route('/', methods=['POST'])
+def hello_world():
+    data = request.get_json()
+    name = data.get('name', 'World')
+    return jsonify({'message': f'Hello, {name}!'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
